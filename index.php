337d7479fd588a60fe94e8fc359aa512284b249a
thefirestrings666 <59292048+thefirestrings666@users.noreply.github.com>
2019-12-27 21:11:24 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<html>
+	<head>
+	</head>
+	<body>
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/7.6.1/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/7.6.1/firebase-analytics.js"></script>
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="/__/firebase/7.6.1/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="/__/firebase/7.6.1/firebase-analytics.js"></script>
+
+<!-- Initialize Firebase -->
+<script src="/__/firebase/init.js"></script>
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAav48KmMiFeRw9bI0KuUIh_pDweXfOAeI",
+    authDomain: "awesomecatsproject.firebaseapp.com",
+    databaseURL: "https://awesomecatsproject.firebaseio.com",
+    projectId: "awesomecatsproject",
+    storageBucket: "awesomecatsproject.appspot.com",
+    messagingSenderId: "679104080678",
+    appId: "1:679104080678:web:deb40010a27b314ca4efe7",
+    measurementId: "G-7JFFYVD26K"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
+	</body>
+</html>
